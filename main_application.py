dcc788bd9b504381433f0c3049713275eaede18b
Redwane Hammas <redwanehammas@gmail.com>
2022-04-10 22:57:12 +0200

    Add second animation

--- main_application.py
+++ main_application.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
+from flask_caching import Cache
 
+cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
 app = Flask(__name__)
+cache.init_app(app)
 
 @app.route('/')
+@cache.cached(timeout=1)
 def home():
     return render_template('home.html')
 
